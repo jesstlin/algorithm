b23f89525ff43a94afc95dbda28503ded1d1f61b
Jessie Lin <tzuhsin.j.lin@gmail.com>
2021-05-08 22:42:01 +0800

    Update schedule_course_recursive.java
    other solutions

--- java/schedule_course_recursive.java
+++ java/schedule_course_recursive.java
@@ -26,3 +26,59 @@ class Schedule_Course_Recursive {
         return memo[i][time];
     }
 }
+
+// Time complexity : O(n^2). We iterate over the countcount array of size nn once. 
+// For every element currently considered, we could scan backwards till the first element, 
+// giving O(n^2) complexity. Sorting the countcount array takes O(nlogn) time for countcount array.
+
+// Space complexity : O(1). Constant extra space is used.
+
+public class Schedule_Course_Iterative {
+    public int scheduleCourse(int[][] courses) {
+        Arrays.sort(courses, (a, b) -> a[1] - b[1]);
+        int time = 0, count = 0;
+        for (int i = 0; i < courses.length; i++) {
+            if ( time + courses[i][0] <= courses[i][1]){
+                time += courses[i][0];
+                 courses[count++] = courses[i];
+            }else{
+                int max_i = i;
+                for (int j=0; j < count; j++){  //find the largest to drop 
+                    if( courses[j][0] > courses[max_i][0] ){
+                        max_i = j;
+                    }
+                }
+                if ( courses[max_i][0] > courses[i][0]){
+                    time +=  courses[i][0]- courses[max_i][0];
+                    courses[max_i] = courses[i];
+                }
+                
+            }
+            
+        }
+        return count;
+    }
+}
+
+
+
+//Time complexity : O(nlogn). At most nn elements are added to the queuequeue. 
+//Adding each element is followed by heapification, which takes O(logn) time.
+//Space complexity : O(n). The queuequeue containing the durations of the courses taken can have atmost nn elements
+public class Schedule_Course_PriorityQueue {
+    public int scheduleCourse(int[][] courses) {
+        Arrays.sort(courses, (a, b) -> a[1] - b[1]);
+        PriorityQueue < Integer > queue = new PriorityQueue < > ((a, b) -> b - a);
+        int time = 0;
+        for (int[] c: courses) {
+            if (time + c[0] <= c[1]) {
+                queue.offer(c[0]);
+                time += c[0];
+            } else if (!queue.isEmpty() && queue.peek() > c[0]) {
+                time += c[0] - queue.poll();
+                queue.offer(c[0]);
+            }
+        }
+        return queue.size();
+    }
+}
