eaf5d3c81addc45ee173e185d71099adb072a7a5
Jessie Lin <tzuhsin.j.lin@gmail.com>
2021-05-10 23:45:28 +0800

    Create Non_Decreasing_Array.java

---
+++ java/Non_Decreasing_Array.java
@@ -0,0 +1,24 @@
+//665. Non-decreasing Array
+//Input: nums = [4,2,3]
+//Output: true
+//Input: nums = [4,2,1]
+//Output: false
+//
+//Guarantee: When decreasing happened, If the previous element in the array is less than the next element, we need to change the current element to the value of the next element
+class Solution {
+    public boolean checkPossibility(int[] nums) {
+        boolean hasChanged = false;
+        for( int i=0; i < nums.length -1; i++){
+            
+            if(nums[i] > nums[i+1]){
+                if(hasChanged) return false;
+                if (i!=0 && nums[i-1] > nums[i+1]){
+                    nums[i+1] = nums[i];
+                }
+                hasChanged = true;
+            }
+            
+        }
+        return true;
+    }
+}
