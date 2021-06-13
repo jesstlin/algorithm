b57f7829acc9e9dcd19ea068da9869d8ed8a9256
Jessie Lin <tzuhsin.j.lin@gmail.com>
2021-06-13 21:17:07 +0800

    Update and rename isPrime.java to CodeExcercise_Middle.java
    Prime , fibonacci

--- java/CodeExcercise_Middle.java
+++ java/CodeExcercise_Middle.java
@@ -1,7 +1,13 @@
 //A prime number is a natural number greater than 1 that has no positive divisors other than 1 and itself.
 //Write a method that checks if a number is a prime number.
 
-public class Prime{
+public class CodeExcercise_Middle{
+  
+  public Integer fibonacci(Integer n) {
+      if ( n == 0 || n ==1 )
+        return n;
+      return fibonacci(n-1) + fibonacci(n-2);
+  }
   
   public Boolean isPrime(Integer n) {
     boolean[] primes = new boolean[n];
