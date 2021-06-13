e8cab87ecee58b4d9acfbcf7f55f3055f69c67d9
Jessie Lin <tzuhsin.j.lin@gmail.com>
2021-06-13 21:49:18 +0800

    Fibonacci, Prime, Palindrome

--- java/CodeExcercise_Middle.java
+++ java/CodeExcercise_Middle.java
@@ -1,14 +1,17 @@
-//A prime number is a natural number greater than 1 that has no positive divisors other than 1 and itself.
-//Write a method that checks if a number is a prime number.
+// Fibonacci, Prime, Palindrome
 
 public class CodeExcercise_Middle{
   
+  
+  //Fn = Fn-1 + Fn-2
   public Integer fibonacci(Integer n) {
       if ( n == 0 || n ==1 )
         return n;
       return fibonacci(n-1) + fibonacci(n-2);
   }
   
+  //A prime number is a natural number greater than 1 that has no positive divisors other than 1 and itself.
+  //Write a method that checks if a number is a prime number.
   public Boolean isPrime(Integer n) {
     boolean[] primes = new boolean[n];
     Arrays.fill(primes, true);
@@ -29,4 +32,19 @@ public class CodeExcercise_Middle{
     }
     return true;
   }
+  
+  
+  // abccfccba
+  public boolean isPalindrome(String word) {
+    int i = 0;
+    int j = word.length() -1;
+    while( j > i){
+      if( word.charAt(i++) !=  word.charAt( j--)){
+            return false;
+        }
+    }
+    return true;
+ }
+  
 }
+
